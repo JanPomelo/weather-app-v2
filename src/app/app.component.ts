a5a8c0a0d3a5623153c0b405efec1847cea210fc
Jan Steikowski <j.steikowski@aol.com>
2023-11-10 14:39:37 +0700

    Add bg image and general styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   standalone: true,
-  selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  template: `
+    <div class="container">
+      <div class="bg"></div>
+      <h1>Hello World!</h1>
+    </div>
+  `,
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'homes';
+  title = "homes";
 }
