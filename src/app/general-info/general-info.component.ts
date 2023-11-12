4092cccd16d9db2348eb056923cf05adc3b9f931
Jan Steikowski <j.steikowski@aol.com>
2023-11-12 10:38:51 +0700

    Add General Info Styles

--- src/app/general-info/general-info.component.ts
+++ src/app/general-info/general-info.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-general-info',
+  selector: "app-general-info",
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      general-info works!
-    </p>
+    <div class="flex flex-col text-white pl-3">
+      <h2 class="text-5xl font-bold">City</h2>
+      <h3 class="text-lg pl-6 mb-3 font-bold">Country</h3>
+      <div class="flex gap-2 items-center">
+        <div class="flex text-xl items-center">
+          <span class="pb-0.5 material-symbols-outlined">Keyboard_Double_Arrow_Down</span>
+          <span>minTemp</span>
+        </div>
+        <p class="text-4xl">curTemp</p>
+        <div class="flex text-xl items-center">
+          <span class="pb-0.5 material-symbols-outlined">Keyboard_Double_Arrow_Up</span>
+          <span>maxTemp</span>
+        </div>
+      </div>
+      <div class="flex gap-3 items-center">
+        <p class="text-xl pl-1 font-bold">conditionText</p>
+        <img src="" alt="" class="h-10" />
+      </div>
+    </div>
   `,
-  styleUrls: ['./general-info.component.css']
+  styleUrls: ["./general-info.component.css"],
 })
-export class GeneralInfoComponent {
-
-}
+export class GeneralInfoComponent {}
